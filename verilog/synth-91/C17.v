544cc12b6c5b79a93a8ccb9c4ad9cb58b591c2e0
canesche <michael.canesche@gmail.com>
2020-10-23 14:15:39 -0300

    update

--- verilog/synth-91/C17.v
+++ verilog/synth-91/C17.v
@@ -1,14 +1,18 @@
+// Benchmark "C17.iscas" written by ABC on Fri Oct 23 14:13:28 2020
+
 module \C17.iscas  ( 
     \1GAT(0) , \2GAT(1) , \3GAT(2) , \6GAT(3) , \7GAT(4) ,
     \22GAT(10) , \23GAT(9)   );
   input  \1GAT(0) , \2GAT(1) , \3GAT(2) , \6GAT(3) , \7GAT(4) ;
   output \22GAT(10) , \23GAT(9) ;
-  wire new_n8_, new_n9_, new_n10_, new_n12_;
-  assign new_n8_ = \1GAT(0)  & \3GAT(2) ;
-  assign new_n9_ = \3GAT(2)  & \6GAT(3) ;
-  assign new_n10_ = \2GAT(1)  & ~new_n9_;
-  assign \22GAT(10)  = new_n8_ | new_n10_;
-  assign new_n12_ = \7GAT(4)  & ~new_n9_;
-  assign \23GAT(9)  = new_n10_ | new_n12_;
+  wire new_new_new_n8___, new_new_new_n9___, new_new_new_n10___,
+    new_new_new_n12___;
+  assign new_new_new_n8___ = \1GAT(0)  & \3GAT(2) ;
+  assign new_new_new_n9___ = \3GAT(2)  & \6GAT(3) ;
+  assign new_new_new_n10___ = ~new_new_new_n9___ & \2GAT(1) ;
+  assign \22GAT(10)  = new_new_new_n8___ | new_new_new_n10___;
+  assign new_new_new_n12___ = ~new_new_new_n9___ & \7GAT(4) ;
+  assign \23GAT(9)  = new_new_new_n10___ | new_new_new_n12___;
 endmodule
+
 
